803b6092423aabfa026ae3d2e95d80b752e933f6
Scott Reynolds <sdrreynolds+gh@gmail.com>
2025-08-17 15:39:48 -0700

    Colorize the output on success scan of the source code
    This uses Ariadne to produce a fun output of the successful scan of
    the source file and break down of the different tokens

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod scanner;
 
-use scanner::{Scanner, Token, TokenInfo};
+use scanner::Scanner;
 use ariadne::Source;
 use std::env;
 use std::fs;
@@ -25,33 +25,47 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 fn tokenize_and_print(content: &str, source_id: &str) -> Result<(), Box<dyn std::error::Error>> {
     let mut scanner = Scanner::new(content, source_id);
 
+    let source = Source::from(content);
+    
     match scanner.scan_all() {
         Ok(tokens) => {
-            for token_info in tokens {
-                print_token(&token_info);
+            let success_report = ariadne::Report::build(ariadne::ReportKind::Advice, (source_id, 0..0))
+                .with_message(format!("Successfully tokenized {} tokens", tokens.len() - 1)) // -1 for EOF
+                .with_note("All tokens parsed successfully");
+            
+            // Add labels for each token with different colors
+            let mut colored_report = success_report;
+            let mut color_gen = ariadne::ColorGenerator::new();
+            
+            for token_info in &tokens {
+                if matches!(token_info.token, scanner::Token::Eof) {
+                    continue;
+                }
+                
+                let color = color_gen.next();
+                let token_type = get_token_type_name(&token_info.token);
+                
+                colored_report = colored_report.with_label(
+                    ariadne::Label::new((source_id, token_info.span.clone()))
+                        .with_message(format!("{}: {:?}", token_type, token_info.token))
+                        .with_color(color)
+                );
             }
+            
+            colored_report.finish().print((source_id, &source))?;
         }
         Err(errors) => {
-            let source = Source::from(content);
             for error in errors {
                 let report = error.into_report();
                 report.print((error.source_id.as_str(), &source))?;
             }
-            std::process::exit(1);
+            return Err("Tokenization failed".into());
         }
     }
 
     Ok(())
 }
 
-fn print_token(token_info: &TokenInfo) {
-    match &token_info.token {
-        Token::Eof => return, // Don't print EOF token
-        token => {
-            println!("{:?} @ {}..{}", token, token_info.span.start, token_info.span.end);
-        }
-    }
-}
 
 fn repl_mode() -> Result<(), Box<dyn std::error::Error>> {
     println!("Jsonnet Scanner REPL - Enter Jsonnet code to tokenize (Ctrl+C to exit)");
@@ -81,23 +95,41 @@ fn repl_mode() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-fn tokenize_and_print_repl(input: &str) {
-    let mut scanner = Scanner::new(input, "repl");
+fn get_token_type_name(token: &scanner::Token) -> &'static str {
+    match token {
+        scanner::Token::Identifier(_) => "Identifier",
+        scanner::Token::Number(_) => "Number", 
+        scanner::Token::String(_) => "String",
+        scanner::Token::Assert => "Keyword",
+        scanner::Token::Else => "Keyword",
+        scanner::Token::Error => "Keyword",
+        scanner::Token::False => "Keyword",
+        scanner::Token::For => "Keyword",
+        scanner::Token::Function => "Keyword",
+        scanner::Token::If => "Keyword",
+        scanner::Token::Import => "Keyword",
+        scanner::Token::ImportStr => "Keyword",
+        scanner::Token::ImportBin => "Keyword",
+        scanner::Token::In => "Keyword",
+        scanner::Token::Local => "Keyword",
+        scanner::Token::Null => "Keyword",
+        scanner::Token::TailStrict => "Keyword",
+        scanner::Token::Then => "Keyword",
+        scanner::Token::Self_ => "Keyword",
+        scanner::Token::Super => "Keyword",
+        scanner::Token::True => "Keyword",
+        scanner::Token::LeftBrace | scanner::Token::RightBrace |
+        scanner::Token::LeftBracket | scanner::Token::RightBracket |
+        scanner::Token::Comma | scanner::Token::Dot |
+        scanner::Token::LeftParen | scanner::Token::RightParen |
+        scanner::Token::Semicolon => "Symbol",
+        scanner::Token::Operator(_) => "Operator",
+        scanner::Token::Eof => "EOF",
+    }
+}
 
-    match scanner.scan_all() {
-        Ok(tokens) => {
-            for token_info in tokens {
-                print_token(&token_info);
-            }
-        }
-        Err(errors) => {
-            let source = Source::from(input);
-            for error in errors {
-                let report = error.into_report();
-                if let Err(e) = report.print((error.source_id.as_str(), &source)) {
-                    eprintln!("Failed to print error: {}", e);
-                }
-            }
-        }
+fn tokenize_and_print_repl(input: &str) {
+    if let Err(e) = tokenize_and_print(input, "repl") {
+        eprintln!("Failed to print tokens: {}", e);
     }
 }
